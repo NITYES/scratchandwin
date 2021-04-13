55fabb9a89732f3bb581c6d991d2008c50f7b3a9
NITYES <nkchaurasiya1@gmail.com>
2021-04-13 18:16:18 +0530

    baseurl changed

--- client/src/function/axios.js
+++ client/src/function/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL=process.env.REACT_APP_NODE_ENV==="PRODUCTION" ? "/api" : "http://localhost:4000/api"
+const baseURL="/api" 
 
 
 export default axios.create({
